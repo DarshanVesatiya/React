928c03d148c68562cbb82b2a56b17fb263b6994d
Darshan Vesatiya <darshanvesatiya123@gmail.com>
2020-04-09 17:00:22 +0530

    Task3(parent is accessing child's method) is uploaded.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from './components/ClassClick';
 import Parent from './components/Parent';
-// import Greet from './components/Greet'
-// import Welcome from './components/Welcome'
-// import Hello from './components/Hello'
-// import Counter from './components/Counter';
-// import LifecycleA from './components/LifecycleA';
-// import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Parent />
-        {/* <FunctionClick />
-        <ClassClick /> */}
-        {/* <Greet name="darshan">YOu KNOW WHAT!!</Greet> */}
-        {/* <Welcome></Welcome> */}
-        {/* <Hello /> */}
-        {/* <Counter /> */}
-        {/* <LifecycleA /> */}
-        {/* <Clock /> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+          <Parent />
       </header>
     </div>
   );
